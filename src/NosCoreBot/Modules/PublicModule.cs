837ec8f9d12502f3d1d3380088c6271893979040
0Lucifer0 <erwan.joly@gmail.com>
2019-10-27 11:52:04 +1300

    add reset i18n

--- src/NosCoreBot/Modules/PublicModule.cs
+++ src/NosCoreBot/Modules/PublicModule.cs
@@ -1,9 +1,15 @@
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.ComTypes;
 using Discord;
 using Discord.Commands;
 using System.Threading.Tasks;
+using Amazon;
+using Amazon.Runtime;
+using Amazon.S3;
+using Amazon.S3.Transfer;
 using Discord.WebSocket;
 using NosCoreBot.Enumerations;
 using NosCoreBot.Extensions;
@@ -35,6 +41,42 @@ namespace NosCoreBot.Modules
             {
                 await clone;
             }
+        }
+
+        [Command("reset-i18n")]
+        [Name("reset-i18n")]
+        [Summary("reset i18n file")]
+        [RequireUserPermission(GuildPermission.Administrator)]
+        [RequireBotPermission(GuildPermission.Administrator)]
+        public async Task ResetI18N()
+        {
+            var client = new AmazonS3Client(new BasicAWSCredentials(
+                Environment.GetEnvironmentVariable("S3_ACCESS_KEY"),
+                Environment.GetEnvironmentVariable("S3_SECRET_KEY")), RegionEndpoint.USWest2);
+            var transferUtility = new TransferUtility(client);
+
+            var emptyfile = "{";
+            foreach (var type in Enum.GetNames(typeof(RegionType)))
+            {
+                emptyfile += $"\"{type}\":[],";
+            }
+            emptyfile += "}";
+
+            MemoryStream stream = new MemoryStream();
+            StreamWriter writer = new StreamWriter(stream);
+            writer.Write(emptyfile);
+            writer.Flush();
+
+            TransferUtilityUploadRequest transferUtilityUploadRequest = new TransferUtilityUploadRequest
+            {
+                BucketName = Environment.GetEnvironmentVariable("S3_BUCKET"),
+                Key = Environment.GetEnvironmentVariable("S3_KEY"),
+                FilePath = Environment.GetEnvironmentVariable("S3_PATH"),
+                ContentType = "text/plain",
+                InputStream = stream
+            };
+
+            await transferUtility.UploadAsync(transferUtilityUploadRequest);
         }
     }
 }
