d488f3ac0a4339dc5249d830415c7027ec469c97
0Lucifer0 <erwan.joly@gmail.com>
2020-01-15 19:46:09 +1300

    allow lambda to cleanup translation

---
+++ src/NosCoreBot/Precondition/RequireUserPermissionOrWebhookAttribute.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Discord;
+using Discord.Commands;
+
+namespace NosCoreBot.Precondition
+{
+    public class RequireUserPermissionOrWebhookAttribute : RequireUserPermissionAttribute
+    {
+        private string[] _webhookNames;
+
+        public RequireUserPermissionOrWebhookAttribute(Discord.GuildPermission permission, string[] webhookNames) : base(permission)
+        {
+            _webhookNames = webhookNames;
+        }
+
+        public RequireUserPermissionOrWebhookAttribute(Discord.ChannelPermission permission, string[] webhookNames) : base(permission)
+        {
+            _webhookNames = webhookNames;
+        }
+    
+
+        public override Task<PreconditionResult> CheckPermissionsAsync(
+          ICommandContext context,
+          CommandInfo command,
+          IServiceProvider services)
+        {
+            IGuildUser user = context.User as IGuildUser;
+            if (context.User is IWebhookUser)
+            {
+                if (_webhookNames.Contains(user.Nickname))
+                {
+                    return Task.FromResult(PreconditionResult.FromSuccess());
+                }
+                return Task.FromResult(PreconditionResult.FromError("Command must be used by an authorized webhook."));
+            }
+
+            if (this.GuildPermission.HasValue)
+            {
+                if (user == null)
+                    return Task.FromResult(PreconditionResult.FromError(this.NotAGuildErrorMessage ?? "Command must be used in a guild channel."));
+                if (!user.GuildPermissions.Has(this.GuildPermission.Value))
+                    return Task.FromResult(PreconditionResult.FromError(this.ErrorMessage ?? string.Format("User requires guild permission {0}.", (object)this.GuildPermission.Value)));
+            }
+            Discord.ChannelPermission? channelPermission = this.ChannelPermission;
+            if (channelPermission.HasValue)
+            {
+                ChannelPermissions channelPermissions = !(context.Channel is IGuildChannel channel) ? ChannelPermissions.All((IChannel)context.Channel) : user.GetPermissions(channel);
+                ref ChannelPermissions local = ref channelPermissions;
+                channelPermission = this.ChannelPermission;
+                long num = (long)channelPermission.Value;
+                if (!local.Has((Discord.ChannelPermission)num))
+                {
+                    string reason = this.ErrorMessage;
+                    if (reason == null)
+                    {
+                        channelPermission = this.ChannelPermission;
+                        reason = string.Format("User requires channel permission {0}.", (object)channelPermission.Value);
+                    }
+                    return Task.FromResult(PreconditionResult.FromError(reason));
+                }
+            }
+            return Task.FromResult(PreconditionResult.FromSuccess());
+        }
+    }
+}
